72276c8c293a7506dc2dc23bfeecaecd1e4c2b75
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-07-14 12:43:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUrl("models"),
+  faceapi.nets.faceLandmark68Net.loadFromUrl("models"),
+  faceapi.nets.faceExpressionNet.loadFromUrl("models"),
+]).then(startStreaming);
+
+function startStreaming() {
+  try {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: false,
+      })
+      .then((stream) => {
+        video.srcObject = stream;
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  } catch (err) {
+    alert("Unable to connect device! Error : " + err.message);
+    console.log(err);
+  }
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const dim = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, dim);
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.tinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+
+    const resizedDetections = faceapi.resizeResults(detections, dim);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 50);
+});
